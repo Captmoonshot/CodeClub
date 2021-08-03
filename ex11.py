a059fe0faea0b6f34fa24920f4fdcb1e9e2903b4
Captmoonshot <gygantor@protonmail.com>
2021-08-03 01:04:42 -0700

    updated

---
+++ ex11.py
@@ -0,0 +1,21 @@
+# ex11.py
+# by Zed Shaw
+
+print("How old are you?", end=" ")
+age = input()
+
+print("How tall are you?", end=" ")
+height = input()
+
+print("How much do you weigh?", end=" ")
+weight = input()
+
+print()
+
+print(f"So, you're {age} years old, {height} tall, and {weight} pounds heavy.")
+
+print()
+
+
+
+
