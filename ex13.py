25dcd04d07250f9275b8d0e58a49d2fffe51014b
Captmoonshot <gygantor@protonmail.com>
2021-08-03 21:07:23 -0700

    updated

---
+++ ex13.py
@@ -0,0 +1,15 @@
+# ex13.py
+# by Zed Shaw
+
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+
+
+
