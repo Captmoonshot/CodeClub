6beb4519e551867ceb8cc37b39a8b000f4581f87
Captmoonshot <gygantor@protonmail.com>
2021-08-15 17:40:35 -0700

    updated

--- ex12.py
+++ ex12.py
@@ -1,9 +1,15 @@
 # ex12.py
 # by Zed Shaw
 
+from sys import argv
+
+script, first = argv
+
 age = input("How old are you? ")
 height = input("How tall are  you? ")
 weight = input("How much do you weight? ")
+
+print(f"This is the argv: {first}")
 
 print()
 print(f"So, you're {age} old, {height} tall, and {weight} pounds heavy.")
--- ex13.py
+++ ex13.py
@@ -1,14 +1,15 @@
 # ex13.py
 # by Zed Shaw
 
-from sys import argv
+from sys import argv # argv stands for "argument variable"
 
-script, first, second, third = argv
+script, first, second, third = argv # this is called argument unpacking to variables
 
 print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
+
 
 
 
