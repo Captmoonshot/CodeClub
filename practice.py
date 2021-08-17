2a0fad754f7178193fc0e16c075b123688f841fe
Captmoonshot <gygantor@protonmail.com>
2021-08-17 08:39:07 -0700

    updated

--- practice.py
+++ practice.py
@@ -1,11 +1,23 @@
-from sys import argv
+import random
 
-script, filename = argv
+def generate_random_number():
+    """This function generates a random number
+    in the range 0 to 10 inclusive of the 10."""
+    random_number = random.randint(0, 10)
+    return random_number
 
-in_file = open(filename)
-indata = in_file.read()
 
-to_file = "ex17_sample.txt"
+def random_sentence():
+    number_one = generate_random_number()
+    number_two = generate_random_number()
+    number_three = generate_random_number()
+    print(f"I have {number_one} dollars in my bank account.")
+    print(f"I have {number_two} friends at school.")
+    print(f"I got a {number_three} out of 10 on my math test.")
+
+
+
+
 
 
 
