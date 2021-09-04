23ab4c22dbd6a089b966aa9ee22c4e9a3a403261
Captmoonshot <gygantor@protonmail.com>
2021-09-04 01:15:42 -0700

    updated

--- ex38.py
+++ ex38.py
@@ -1,3 +1,28 @@
+# ex38.py
+# by Zed Shaw
+
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+stuff = ten_things.split(" ")
+more_stuff = ["Day", "Night", "Song", "Frisbee",
+    "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print("Adding: ", next_one)
+    stuff.append(next_one)
+    print(f"There {len(stuff)} items now.")
+
+print("There we go: ", stuff)
+
+print("Let's do some things with stuff")
+
+print(stuff[1])
+print(stuff[-1]) # whoa! fancy
+print(stuff.pop())
+print(" ".join(stuff)) # what? cool!
+print("#".join(stuff[3:5])) # super stellar!
+
 
 
 
