7596feb40bd3646f27316e8ffd8284a33842a00e
Captmoonshot <gygantor@protonmail.com>
2021-09-05 16:06:11 -0700

    udpated

---
+++ ex40a.py
@@ -0,0 +1,26 @@
+# ex40a.py
+# by Zed Shaw
+
+import mystuff
+
+mystuff.apple()
+print(mystuff.tangerine)
+
+#####################################################
+
+class MyStuff:
+
+    def __init__(self):
+        self.tangerine = "An now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY-APPLES!")
+
+
+
+
+
+
+
+
+
